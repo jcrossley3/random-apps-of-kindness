9568250f7beb9a4860f327fbf91212e1058ea92b
Jim Crossley <jim@crossleys.org>
2013-03-12 15:49:59 -0400

    xa

--- demo/project.clj
+++ demo/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring/ring-core "1.2.0-beta1"]]
+                 [ring/ring-core "1.2.0-beta1"]
+                 [com.h2database/h2 "1.3.160"]]
   :immutant {:nrepl-port 7888})
